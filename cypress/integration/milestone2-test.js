271fa8fa92e7f19bca3e3da4b04a18deab47d7ab
brianwhelandublin <brianwhelan1984@yahoo.co.uk>
2020-11-21 16:47:14 +0000

    add home page test

--- cypress/integration/milestone2-test.js
+++ cypress/integration/milestone2-test.js
@@ -1,1 +1,15 @@
-cy
+describe("Test Homepage", function() {
+    it("Visits milestone project", function() {
+        cy.visit("https://brianwhelandublin.github.io/milestone-project-2/index.html")
+    })
+    it("Tests Navigation", function() {
+        cy.get(".navigation-list").should("be.hidden")
+            // click hamburger button should open nav
+        cy.get(".hamburger").click()
+        cy.get(".navigation-list").should("be.visible")
+            // click x button should close nav
+        cy.get(".hamburger").click()
+        cy.get(".navigation-list").should("be.hidden")
+    })
+
+})
