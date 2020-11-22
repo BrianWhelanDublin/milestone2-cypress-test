96169c599b243e165bcbaa38c155544b297d8534
brianwhelandublin <brianwhelan1984@yahoo.co.uk>
2020-11-22 15:01:44 +0000

    write landing page tests

--- cypress/integration/milestone2-test.js
+++ cypress/integration/milestone2-test.js
@@ -1,8 +1,8 @@
-describe("Test Homepage", function() {
-    it("Visits milestone project", function() {
+describe("Test Homepage", () => {
+    it("Visits milestone project", () => {
         cy.visit("https://brianwhelandublin.github.io/milestone-project-2/index.html")
     })
-    it("Tests Navigation", function() {
+    it("Tests Navigation", () => {
         cy.get(".navigation-list").should("be.hidden")
             // click hamburger button should open nav
         cy.get(".hamburger").click()
@@ -11,5 +11,79 @@ describe("Test Homepage", function() {
         cy.get(".hamburger").click()
         cy.get(".navigation-list").should("be.hidden")
     })
+    it("Tests title", () => {
+        cy.get(".main-title").should("contain", "Great Escapes")
+        cy.get(".hero-text")
+            // test find out more button
+        cy.get(".hero-link")
+            .should("contain", "Find Out More")
+            .and("have.attr", "href",
+                "#about")
+    })
+    it("Tests about section", () => {
+        cy.get(".hero-link").click().url().should("include", "#about")
+        cy.get(".about-col").find("img").should("have.attr", "src").should("contain", "assets/images/about.jpg")
+        cy.get(".about-heading").should("contain", "Explore The World")
+    })
+    it("Tests Our destination Section", () => {
+        cy.get("#destinations").scrollIntoView()
+        cy.get(".destination-heading").should("contain", "Our Destinations")
+        cy.get("#cities-slider").scrollIntoView()
+        cy.get("#cities-slider").children(".destination-card")
+        cy.get("#beach-slider").scrollIntoView()
+        cy.get("#beach-slider").children(".destination-card")
+    })
+    it("Tests Reviews section", () => {
+        cy.get("#reviews").scrollIntoView()
+        cy.get(".reviews-heading").should("contain", "Our Reviews")
+        cy.get(".reviewer-1").should("have.class", "active")
+        cy.get(".review-2").should("be.hidden")
+        cy.get(".review-3").should("be.hidden")
+        cy.get(".reviewer-2").click().then(() => {
+            cy.get(".reviewer-2").should("have.class", "active")
+            cy.get(".review-1").should("be.hidden")
+            cy.get(".review-3").should("be.hidden")
+        })
+        cy.get(".reviewer-3").click().then(() => {
+            cy.get(".reviewer-3").should("have.class", "active")
+            cy.get(".review-1").should("be.hidden")
+            cy.get(".review-2").should("be.hidden")
+        })
+        cy.get(".reviewer-1").click().then(() => {
+            cy.get(".reviewer-1").should("have.class", "active")
+            cy.get(".review-2").should("be.hidden")
+            cy.get(".review-3").should("be.hidden")
+        })
+    })
+    it("Tests Offers Section", () => {
+        cy.get("#special-packages").scrollIntoView()
+        cy.get("#special-packages").should("contain", "Our Package Offers")
+
+    })
+    it("Tests Newsletter Section", () => {
+        cy.get("#signup").scrollIntoView()
+        cy.get("#signup-email").should("have.attr", "placeholder", "Email")
+        cy.get("#newsletterModal").should("be.hidden")
+        cy.get("#signup-email").type("email@email.com")
+            // cy.get("#signup-submit").click()
+            //cy.wait(500).then(() => {
+            //     cy.get("#newsletterModal").should("be.visible")
+            //})
+    })
+    it("Test Contact Form Section", () => {
+        cy.get("#contact").scrollIntoView()
+        cy.get("#contactformModal").should("be.hidden")
+        cy.get("#contact-name").should("have.attr", "placeholder", "Name").type("name")
+        cy.get("#contact-email").should("have.attr", "placeholder", "Email").type("email@email.com")
+        cy.get("#contact-message").should("have.attr", "placeholder", "Message").type("message")
+            // cy.get(".contact-btn").click()
+            //cy.wait(500).then(() => {
+            //     cy.get("#contactFormModal").should("be.visible")
+            //})
+    })
+    it("Tests footer", () => {
+        cy.get(".social-media-links").should("have.attr", "target", "_blank")
+    })
+
 
 })
